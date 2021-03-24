2496d3a03c365b8cd126f95cb0c6a53d2a408b29
Isabel Matos <65324450+isabelpmatos@users.noreply.github.com>
2021-03-24 19:19:20 -0300

    alterações js

--- codigo/index.js
+++ codigo/index.js
@@ -1,11 +1,30 @@
 const express = require("express")
-
 const app = express()
 
-app.get("/", (req, res) => {
-    res.render("./index.html")
+const bodyParser = require("body-parser"); //traduzir dados enviados em uma estrutura js
+const connection = require("./database/database");
+const Usuario = require("./database/users");
 
+//Database
+connection
+    .authenticate()
+    .then(() => {
+        console.log("Conexão feita com sucesso");
+    })
+    .catch((msgErro) => {
+        console.log(msgErro);
+    })
+
+
+app.get("/", (req, res) => {
+    res.render("index")
 })
+
+app.set('view engine', 'ejs')
+app.use(express.static('public'))
+
+app.use(bodyParser.urlencoded({extended: false}));
+app.use(bodyParser.json());
 
 app.listen(5001, () => {
     console.log("Server is started")
