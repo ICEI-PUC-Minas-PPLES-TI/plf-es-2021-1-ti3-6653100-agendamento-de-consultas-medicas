292454c89f0b942fbb99c43d9a08c07459f11505
Isabel Matos <65324450+isabelpmatos@users.noreply.github.com>
2021-03-25 23:18:59 -0300

    backend sprint 1 concluído

--- codigo/index.js
+++ codigo/index.js
@@ -20,6 +20,10 @@ app.get("/", (req, res) => {
     res.render("index")
 })
 
+app.get("/home", (req, res) => {
+    res.render("home")
+})
+
 app.set('view engine', 'ejs')
 app.use(express.static('public'))
 
@@ -28,4 +32,27 @@ app.use(bodyParser.json());
 
 app.listen(5001, () => {
     console.log("Server is started")
+})
+
+app.post("/", (req, res) => {
+
+    var email = req.body.email;
+    var senha = req.body.senha;
+
+    Usuario.findOne({where: {email: email}}).then(usuario => {
+
+        if(usuario != undefined){
+
+            if(usuario.senha == senha){
+                res.redirect("/home")
+            }
+            else{
+                res.redirect("/");
+            }
+        }
+        else{
+            res.redirect("/");
+        }
+    })
+
 })
