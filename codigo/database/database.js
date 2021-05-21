6951bf5e59ea740ff45847653bfacc450a3c5c78
Isabel Matos <65324450+isabelpmatos@users.noreply.github.com>
2021-05-21 03:37:43 -0300

    relacionamento paciente x consulta

--- codigo/database/database.js
+++ codigo/database/database.js
@@ -2,7 +2,8 @@ const Sequelize = require ('sequelize'); //importando módulo
 
 const connection = new Sequelize('tis3', 'root', 'bel78477847', {
     host: 'localhost', 
-    dialect: 'mysql'
+    dialect: 'mysql' ,
+    timezone: "-03:00"
 });
 
 module.exports = connection;
